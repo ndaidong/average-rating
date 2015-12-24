fafb78a15068a59e358701b629e60b1b3c1462bc
Dong Nguyen <ndaidong@gmail.com>
2015-12-24 12:56:56 +0700

    Update conversion

--- index.js
+++ index.js
@@ -1,2 +1,8 @@
+/**
+ * Starting app
+ * @ndaidong
+**/
+'use strict';
+
 exports = module.exports = require('./src/average-rating');
 exports.version = require('./package').version;
--- src/average-rating.js
+++ src/average-rating.js
@@ -7,10 +7,10 @@
 'use strict'; // to use "let" keyword
 
 var getWilsonScore = (p, n) => {
-  if(p === 0 && n === 0){
+  if (p === 0 && n === 0) {
     return 0;
   }
-  let r = ((p + 1.9208) / (p + n) - 1.96 * Math.sqrt((p * n) / (p + n) + 0.9604) / (p + n)) / (1 + 3.8416 / (p + n));
+  let r = ((p + 1.9208) / (p + n) - 1.96 * Math.sqrt(p * n / (p + n) + 0.9604) / (p + n)) / (1 + 3.8416 / (p + n));
   return r.toFixed(2);
 }
 
@@ -34,7 +34,7 @@ module.exports = {
     let total = rating.reduce((prev, current) => {
       return prev + current;
     });
-    if(total === 0){
+    if (total === 0) {
       return 0;
     }
     let sum = 0, k = 1;
